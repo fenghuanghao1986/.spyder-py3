74c199bb34db4280a1caa01142c18419204c15d6
FingerStyleFinn <hfeng7@du.edu>
2018-04-26 02:15:09 -0600

    Finishing 3 questions
    creating Bar chart
    fixed some problem
    
    but the plt.bar still not look right.
    try tomorrow

--- STATInClassWork1.py
+++ STATInClassWork1.py
@@ -27,6 +27,7 @@ print(stat.head(5))
 # part a. Summary statics
 statSummary = stat.describe()
 # part b. 
+
 """
 This part is for several histograms for different col and different dataframe
 # numCalls histogram for original dataframe
@@ -38,6 +39,7 @@ SaleHist = stat.Sales.hist()
 # sales histogram for statics 
 summSaleHist = statSummary.Sales.hist()
 """
+
 # I assume you want us to creat histograms from original dataframe
 stat[["NumCalls","Sales"]].hist() # on your slides page 15 the format you write was wrong, you were missing one set of []
 # part c. 
@@ -60,19 +62,26 @@ plt.show()
 # part a. Summary statics
 # I am not sure what is the difference between Q2 part a
 statSummary = stat.describe()
-# I assume you want us to creat histograms from original dataframe
-stat[["NumCalls","Sales"]].plot() # on your slides page 15 the format you write was wrong, you were missing one set of []
+# create call number bar chart 
+callBar = stat.NumCalls.value_counts()
+callBar = pd.DataFrame(callBar)
+callBar.plot(kind="bar")
+# create sale bar chart
+saleBar = stat.Sales.value_counts()
+saleBar = pd.DataFrame(saleBar)
+saleBar.plot(kind="bar")
 # part c. 
+# use matplot to create bar chart
 plt.subplot(211)
 plt.grid(True)
-plt.plot(stat.NumCalls)
-plt.title('Histogram of Calls')
+plt.bar(stat.NumCalls, np.array(len(stat.NumCalls)))
+plt.title('Barchart of Calls')
 plt.xlabel('Number of calls')
 plt.ylabel('Student Number')
 plt.subplot(212)
 plt.grid(True)
-plt.plot(stat.Sales)
-plt.title('Histogram of Sales')
+plt.bar(stat.Sales, np.array(len(stat.Sales)))
+plt.title('Barchart of Sales')
 plt.xlabel('Sales Values')
 plt.ylabel('Student number')
 plt.subplots_adjust(hspace= 0.6)
